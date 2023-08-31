db8582df52e245fad50f65f2fa86b6b001a67b0f
dancg <dannycalgon@gmail.com>
2023-08-31 11:28:39 +0200

    Añado más ejemplos de marcas y artículos

--- database/seeders/ArticleSeeder.php
+++ database/seeders/ArticleSeeder.php
@@ -33,5 +33,41 @@ class ArticleSeeder extends Seeder
             'marca_id'=> '2', //La id de Garnier es 2 (es una prueba creada manualmente)
             'category_id'=> '2',
         ]);
+        Article::create([
+            'nombre' => 'Maquina de pelar',
+            'descripcion' => 'es una máquina de pelar Philips para hombres.',
+            'disponible'=>'SI',
+            'imagen' => 'maquinaphilips.webp',
+            'precio' => 25 ,
+            'marca_id'=> '3',
+            'category_id'=> '1',
+        ]);
+        Article::create([
+            'nombre' => 'Plancha de pelo',
+            'descripcion' => 'es una plancha de pelo de la marca Regminton.',
+            'disponible'=>'NO',
+            'imagen' => 'plancharemington.webp',
+            'precio' => 19.99 ,
+            'marca_id'=> '4',
+            'category_id'=> '3',
+        ]);
+        Article::create([
+            'nombre' => 'Acondicionador',
+            'descripcion' => 'es un acondicionador para mujeres.',
+            'disponible'=>'SI',
+            'imagen' => 'acondicionadorloreal.jpg',
+            'precio' => 30 ,
+            'marca_id'=> '5',
+            'category_id'=> '2',
+        ]);
+        Article::create([
+            'nombre' => 'Tinte',
+            'descripcion' => 'es un producto para tintar el pelo para mujeres.',
+            'disponible'=>'SI',
+            'imagen' => 'tintegarnier.jpg',
+            'precio' => 20 ,
+            'marca_id'=> '2',
+            'category_id'=> '2',
+        ]);
     }
 }
--- database/seeders/MarcaSeeder.php
+++ database/seeders/MarcaSeeder.php
@@ -16,16 +16,40 @@ class MarcaSeeder extends Seeder
     {
         Marca::create([
             'nombre' => 'Pantene',
-            'descripcion' => 'es una marca registrada de productos para cuidado del cabello, 
+            'descripcion' => 'Es una marca registrada de productos para cuidado del cabello, 
             producido por Procter & Gamble.',
             'imagen' => 'pantene.jpg'
         ]);
 
         Marca::create([
             'nombre' => 'Garnier',
-            'descripcion' => "es una marca de cosméticos de mercado masivo de la compañía 
+            'descripcion' => "Es una marca de cosméticos de mercado masivo de la compañía 
             francesa de cosméticos LOréal. Produce productos para el cuidado del cabello y la piel.",
             'imagen' => 'garnier.jpg'
+        ]);
+
+        Marca::create([
+            'nombre' => 'Philips',
+            'descripcion' => " Es una empresa neerlandesa de tecnología fundada en la ciudad de Eindhoven 
+            en 1891. Es considerada una de las más grandes e importantes del mundo en su campo. 
+            Está dedicada, principalmente, a los sectores de la electrónica y la asistencia sanitaria.",
+            'imagen' => 'philips.jpg'
+        ]);
+
+        Marca::create([
+            'nombre' => 'Remington',
+            'descripcion' => "Es una marca estadounidense de cuidado personal que fabrica cortapelos, 
+            afeitadoras eléctricas, depiladoras y productos para el cuidado del cabello. 
+            Es una subsidiaria de Spectrum Brands y Oak Hill Capital.",
+            'imagen' => 'regminton.jpg'
+        ]);
+
+        Marca::create([
+            'nombre' => "L´Oreal",
+            'descripcion' => "L´Oréal es una empresa francesa de cosméticos y belleza, creada en 1909 
+            por el químico Eugène Schueller. Con sede en Clichy, es la compañía de cosméticos 
+            más grande del mundo, y cuenta con una sede social en París.",
+            'imagen' => 'loreal.jpg'
         ]);
     }
 }
