e3b0d45c0bdf8bbc1d87bb6f1bcbb58efd463ba5
dancg <dannycalgon@gmail.com>
2023-10-03 16:23:07 +0200

    Prueba Commit

--- resources/views/livewire/show-marcas.blade.php
+++ resources/views/livewire/show-marcas.blade.php
@@ -3,7 +3,7 @@
         <nav aria-label="Migas de Pan (Breadcrumbs)" class="mb-2 ml-2">
             <ol class="list-none p-0 inline-flex">
                 <li class="flex items-center">
-                    <a href="/" class="hover:text-blue-700 text-blue-900">Inicio</a>
+                    <a href="/" class="hover:text-blue-700 text-blue-900">Inicio</a> 
                 </li>
                 <li class="mx-2">
                     <i class="fa-solid fa-chevron-right "></i>
